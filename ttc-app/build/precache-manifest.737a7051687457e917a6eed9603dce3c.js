1abc0644f44d8b417f814eb2904e6ee0740ce2a7
Troy Moore <tbmoore85@gmail.com>
2021-02-03 19:55:05 -0500

    added build file for netlify deploy

---
+++ ttc-app/build/precache-manifest.737a7051687457e917a6eed9603dce3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6527c50449dfc3c4336174c88b40a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cbd91396c5fc0f903e1",
+    "url": "/static/css/main.8e24140a.chunk.css"
+  },
+  {
+    "revision": "0296d976122590f396c9",
+    "url": "/static/js/2.e4c81d38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e4c81d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbd91396c5fc0f903e1",
+    "url": "/static/js/main.de866925.chunk.js"
+  },
+  {
+    "revision": "58139d3013058178b8cd",
+    "url": "/static/js/runtime-main.50c34152.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
